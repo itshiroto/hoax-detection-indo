9e1286ee44ef85ba82ebc05119b8406022af8a76
Rivo Juicer Wowor <rivowowor7@gmail.com>
2025-04-09 22:27:50 +0700

    fix: update default dataset path to hoax_1k.csv in config

--- umn_hoax_detect/config.py
+++ umn_hoax_detect/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Path to your Mafindo CSV dataset
-DATASET_PATH = os.getenv("DATASET_PATH", "data/hoax_1k.csv")
+DATASET_PATH = os.getenv("DATASET_PATH", "hoax_1k.csv")
 
 # Columns to extract
 COLUMNS = ["title", "content", "fact", "conclusion"]
